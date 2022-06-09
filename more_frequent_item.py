5cb14d2a6d8aef9106e37f77945d65a84b3e3ce4
chrysanthemum <86790531+anthonysingiser@users.noreply.github.com>
2022-06-09 16:06:18 -0400

    add challenge

---
+++ more_frequent_item.py
@@ -0,0 +1,13 @@
+# Create a function named more_frequent_item that has 
+# three parameters named lst, item1, and item2.
+
+# Return either item1 or item2 depending on which 
+# item appears more often in lst.
+
+def more_frequent_item(lst, item1, item2):
+  if lst.count(item1) >= lst.count(item2):
+    return item1
+  else:
+    return item2
+
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
